02f189f4fa8ffacc5426ebd31a124a192e1c6b98
jefflike <2609749420@qq.com>
2018-04-06 15:17:49 +0800

    个人主页和头像

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, Manager, MigrateCommand
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
 login = LoginManager(app)
 login.login_view = 'login'
 
